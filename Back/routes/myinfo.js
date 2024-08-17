f9976b9e38e9e544980199d02082453547f78418
01LEE <tkaqkrrhf11@gmail.com>
2024-08-17 13:03:25 +0900

    updatetime 13:03

--- Back/routes/myinfo.js
+++ Back/routes/myinfo.js
@@ -5,11 +5,11 @@ const myinfoService = require('../services/myinfoService');
 
 router.get('/getmyinfo', isAuthenticated, myinfoService.getMyInfo);
 router.post('/updatemyinfo', isAuthenticated, myinfoService.updateMyInfo);
-router.get('/addpet', isAuthenticated, myinfoService.renderaddpet);
+// router.get('/addpet', isAuthenticated, myinfoService.renderaddpet);
 // router.post addpet은 profile.js에서 한 번에 보냄
-//router.post('/addpet', isAuthenticated, myinfoService.updateaddpet);
+router.post('/addpet', isAuthenticated, myinfoService.updateaddpet);
 router.get('/mypets', isAuthenticated, myinfoService.mypets);
-router.get('/deleteuser', isAuthenticated, myinfoService.deleteuser);
+// router.get('/deleteuser', isAuthenticated, myinfoService.deleteuser);
 
 router.get('/pet/:pet_id', isAuthenticated, myinfoService.petdetail);
 router.post('/pet/:pet_id', isAuthenticated, myinfoService.updatepetdetail);
--- Back/routes/profile.js
+++ Back/routes/profile.js
@@ -1,24 +1,12 @@
-// routes/profileRouter.js
 const express = require('express');
 const router = express.Router();
 const profileService = require('../services/profileService');
-const myinfoService = require('../services/myinfoService');
 const { multer, uploadPetImage } = require('../middlewares/uploadimage');
 
-router.get('/upload', (req, res) => {
-    res.render('upload');
-});
+// 프로필 이미지 업로드 라우트
 router.post('/upload', profileService.multer.single('profileImage'), profileService.uploadProfileImage);
 
-// router.get('/petupload', (req,res) => {
-//     res.render('/petupload');
-// })
-router.post('/petupload', multer.single('petImage'), uploadPetImage, (req, res, next) => {
-    console.log('Request Body:', req.body); // 디버깅 로그 추가
-    next();
-  }, myinfoService.updateaddpet);
+// 반려동물 이미지 업로드 라우트
+router.post('/petupload', multer.single('petImage'), uploadPetImage, profileService.updatePetImage);
 
-  router.post('/updatepetimage/:pet_id', multer.single('petImage'), uploadPetImage, myinfoService.updatePetImage);
-
-  
 module.exports = router;
